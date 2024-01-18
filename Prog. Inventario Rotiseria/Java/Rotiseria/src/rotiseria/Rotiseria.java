ce87c025b1c3b1d48c660e9b2488f688b52530a6
ArnaldoRom <142050350+ArnaldoRom@users.noreply.github.com>
2024-01-18 19:16:26 -0300

    Initial commit

---
+++ Prog. Inventario Rotiseria/Java/Rotiseria/src/rotiseria/Rotiseria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rotiseria;
+
+/**
+ *
+ * @author pablo
+ */
+public class Rotiseria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
